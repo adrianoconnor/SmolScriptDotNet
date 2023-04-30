11a340a8d9abc352eae2051dcf1b92c9a79c5a21
Adrian O'Connor <adrian.oconnor@gmail.com>
2023-04-30 22:53:56 +0100

    More equality tests

--- SmolScript.Tests.Internal/Types/Equality.cs
+++ SmolScript.Tests.Internal/Types/Equality.cs
@@ -46,4 +46,49 @@ public class Equality
         Assert.IsFalse(a == b);
     }
 
+    [TestMethod]
+    public void SmolBoolEquality()
+    {
+        var vm = SmolVM.Init("var a = true; var b = false; var c = a == b; var d = true;");
+
+        Assert.IsTrue(vm.GetGlobalVar<bool>("a"));
+        Assert.IsFalse(vm.GetGlobalVar<bool>("b"));
+        Assert.IsFalse(vm.GetGlobalVar<bool>("c"));
+        Assert.IsTrue(vm.GetGlobalVar<bool>("d"));
+
+        // We will get the values as a mixture of base type (which is how
+        // they're defined in the stack) and actual type (which is how
+        // they're often used internally) -- this let's us check that
+        // comparison when using base types is ok
+        var a = ((SmolVM)vm).globalEnv.Get("a") as SmolBool;
+        var b = ((SmolVM)vm).globalEnv.Get("b") as SmolBool;
+        var c = ((SmolVM)vm).globalEnv.Get("c") as SmolStackValue;
+        var d = ((SmolVM)vm).globalEnv.Get("d") as SmolStackValue;
+
+        Assert.IsInstanceOfType<SmolBool>(a);
+        Assert.IsInstanceOfType<SmolBool>(b);
+        Assert.IsInstanceOfType<SmolBool>(c);
+        Assert.IsInstanceOfType<SmolBool>(d);
+
+        Assert.IsInstanceOfType<SmolStackValue>(a);
+        Assert.IsInstanceOfType<SmolStackValue>(b);
+        Assert.IsInstanceOfType<SmolStackValue>(c);
+        Assert.IsInstanceOfType<SmolStackValue>(d);
+
+        Assert.AreEqual(a, d);
+        Assert.AreEqual(a.GetValue(), d.GetValue());
+
+        Assert.IsTrue(a == d);
+        Assert.IsFalse(a != d);
+
+        // GetValue returns an (optional) object, so we need to cast to
+        // the actual type if we want to compare directly, otherwise will
+        // never be equal (becuase they aren't the same instance)
+        Assert.IsTrue((bool)a.GetValue()! == (bool)d.GetValue()!);
+
+        Assert.AreNotEqual(a, b);
+        Assert.IsTrue(a != b);
+        Assert.IsFalse(a == b);
+    }
+
 }
--- SmolScript/Internals/SmolProgram.cs
+++ SmolScript/Internals/SmolProgram.cs
@@ -5,12 +5,12 @@ namespace SmolScript.Internals
 {
     internal struct SmolProgram
     {
-        public List<SmolStackValue> constants { get; set; }
-        public List<List<ByteCodeInstruction>> code_sections { get; set; }
-        public List<SmolFunction> function_table { get; set; }
-        public Dictionary<string, string?> class_table { get; set; }
+        internal List<SmolStackValue> constants { get; set; }
+        internal List<List<ByteCodeInstruction>> code_sections { get; set; }
+        internal List<SmolFunction> function_table { get; set; }
+        internal Dictionary<string, string?> class_table { get; set; }
 
-        public IList<Statement> astStatements { get; set; }
+        internal IList<Statement> astStatements { get; set; }
     }
 }
 
