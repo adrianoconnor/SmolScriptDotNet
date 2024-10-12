4fc44c6b20468611eeb8d57cb30487ffbd977bdb
Adrian O'Connor <adrian.oconnor@gmail.com>
2024-10-12 23:57:51 +0100

    Added a float specific test to satisfy my curiosity about something...

--- SmolScript.Tests.Internal/Types/Equality.cs
+++ SmolScript.Tests.Internal/Types/Equality.cs
@@ -48,6 +48,58 @@ public class Equality
     }
 
     [TestMethod]
+    public void SmolNumberEqualityFloat()
+    {
+        var vm = SmolVM.Init("var a = 0.1; var b = 0.2; var c = 0.1; var d = a + b; var e = d == 0.3;");
+
+        Assert.AreEqual(0.1M, vm.GetGlobalVar<decimal>("a"));
+        Assert.AreEqual(0.2M, vm.GetGlobalVar<decimal>("b"));
+        Assert.AreEqual(0.1M, vm.GetGlobalVar<decimal>("c"));
+        Assert.AreEqual(0.3M, vm.GetGlobalVar<decimal>("d"));
+
+        Assert.AreEqual(0.1, vm.GetGlobalVar<double>("a"));
+        Assert.AreEqual(0.2, vm.GetGlobalVar<double>("b"));
+        Assert.AreEqual(0.1, vm.GetGlobalVar<double>("c"));
+        Assert.AreEqual(0.30000000000000004, vm.GetGlobalVar<double>("d"));
+
+        // We will get the values as a mixture of base type (which is how
+        // they're defined in the stack) and actual type (which is how
+        // they're often used internally) -- this let's us check that
+        // comparison when using base types is ok
+        var a = ((SmolVM)vm).globalEnv.Get("a") as SmolNumber;
+        var b = ((SmolVM)vm).globalEnv.Get("b") as SmolNumber;
+        var c = ((SmolVM)vm).globalEnv.Get("c") as SmolVariableType;
+        var d = ((SmolVM)vm).globalEnv.Get("d") as SmolNumber;
+        var e = ((SmolVM)vm).globalEnv.Get("e") as SmolBool;
+        
+        Assert.IsInstanceOfType<SmolNumber>(a);
+        Assert.IsInstanceOfType<SmolNumber>(b);
+        Assert.IsInstanceOfType<SmolNumber>(c);
+
+        Assert.IsInstanceOfType<SmolVariableType>(a);
+        Assert.IsInstanceOfType<SmolVariableType>(b);
+        Assert.IsInstanceOfType<SmolVariableType>(c);
+
+        Assert.AreEqual(a, c);
+        Assert.AreEqual(a.GetValue(), c.GetValue());
+
+        Assert.IsTrue(a == c);
+        Assert.IsFalse(a != c);
+
+        // GetValue returns an (optional) object, so we need to cast to
+        // the actual type if we want to compare directly, otherwise will
+        // never be equal (becuase they aren't the same instance)
+        Assert.IsTrue((double)a.GetValue()! == (double)c.GetValue()!);
+
+        Assert.AreNotEqual(a, b);
+        Assert.IsTrue(a != b);
+        Assert.IsFalse(a == b);
+
+        Assert.AreEqual(0.30000000000000004, d.value);
+        Assert.IsFalse(e.value);
+    }
+
+    [TestMethod]
     public void SmolBoolEquality()
     {
         var vm = SmolVM.Init("var a = true; var b = false; var c = a == b; var d = true;");
